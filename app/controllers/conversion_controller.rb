a76a1107d89fefc84ab0470bd55a635d80ce391c
omar-amrani <omar.amrani@intercom.io>
2019-08-20 12:12:35 +0100

    Reformating the code

--- app/controllers/conversion_controller.rb
+++ app/controllers/conversion_controller.rb
@@ -17,6 +17,7 @@ class ConversionController < ApplicationController
     end
   end
 
+  # convert takes an integer and return the corresponding roman numeral
   def convert(i)
     roman_symbols_hash = {1 => "I", 4 => "IV", 5 => "V", 9 => "IX", 10 => "X", 40 => "XL", 50 => "L", 90 => "XC", 100 => "C", 400 => "CD", 500 => "D", 900 => "CM", 1000 => "M"}
     roman_numeral = ''
@@ -30,6 +31,7 @@ class ConversionController < ApplicationController
     return roman_numeral
   end
 
+  #is_param_integer? method checks whether the input is an integer or not
   def is_param_integer?(param)
     param.to_i.to_s == param
   end
--- spec/controllers/conversion_controller_spec.rb
+++ spec/controllers/conversion_controller_spec.rb
@@ -11,21 +11,25 @@ describe ConversionController, type: :controller do
 
 
   describe "return_roman_numeral" do
+
     context 'The integer input is valid' do
+
       it "Should return CXIII if integer input is 113 then render new template" do
         get :return_roman_numeral, params: {integer_input: valid_integer_1}
         expect(assigns(:roman_numeral)).to eq("CXIII")
         expect(response).to render_template("new")
       end
+
       it "Should return MMMDCXLIV if integer input is 3644 then render new template" do
         get :return_roman_numeral, params: {integer_input: valid_integer_2}
         expect(assigns(:roman_numeral)).to eq("MMMDCXLIV")
         expect(response).to render_template("new")
-
       end
+
     end
 
     context 'The input is not an integer' do
+
       it "Should redirect to root path with a flash danger message if input is not an integer" do
         get :return_roman_numeral, params: {integer_input: non_integer_input}
         expect(flash[:danger]).to be_present
@@ -37,9 +41,11 @@ describe ConversionController, type: :controller do
         expect(flash[:danger]).to be_present
         expect(response).to redirect_to(root_path)
       end
+
     end
 
     context 'The integer input is out of range' do
+
       it "Should redirect to root path with a flash danger message if value is zero" do
         get :return_roman_numeral, params: {integer_input: zero}
         expect(flash[:danger]).to be_present
@@ -51,6 +57,7 @@ describe ConversionController, type: :controller do
         expect(flash[:danger]).to be_present
         expect(response).to redirect_to(root_path)
       end
+
     end
   end
 end
